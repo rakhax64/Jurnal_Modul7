3606d18b415cee4f9beb9922132b31c88b2c6087
RakhaPW <rakhapratama@student.telkomuniversity.ac.id>
2022-04-02 14:04:21 +0700

    menambahkan class

---
+++ Modul5/Modul5/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul5
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo saya = new SayaTubeVideo("Tutorial Design By Contract - Rakha Pratama Wahyudi");
+            saya.IncreasePlayCount(0);
+            saya.PrintVideoDetails();
+
+            Console.WriteLine();
+
+            SayaTubeVideo saya1 = new SayaTubeVideo("Rakha Pratama Wahyudi");
+            saya1.IncreasePlayCount(1000000000);
+            saya1.PrintVideoDetails();
+        }
+    }
+}
--- modul6/modul6/Program.cs
+++ modul6/modul6/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace modul6
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-        }
-    }
+			String jsonString = File.ReadAllText("D:\Perkuliahan\Semester 6\Konstruksi Perangkat Lunak\Pratikum\modul6\modul6");
+
+			dynamic data = JsonConvert.DeserializeObject(jsonString);
+
+
+
+			Console.WriteLine("Nama : " + data.firstName + " " + data.lastName);
+			Console.WriteLine("Gender : " + data.gender);
+			Console.WriteLine("Umur : " + data.age);
+			Console.WriteLine("Alamat : " + data.address.streetAddress + ", " + data.address.city + ", " + data.address.state);
+			Console.WriteLine("Mata Kuliah : ");
+			foreach (var item in data.courses)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Kode matkul : " + item.code);
+				Console.WriteLine("Nama matkul : " + item.name);
+			}
+			Console.WriteLine();
+		}
+	}
 }
