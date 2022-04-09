3aeb0fc3027eaa509fdcb0d01f5b741982282260
rakhax64 <95220908+rakhax64@users.noreply.github.com>
2022-04-09 13:35:21 +0700

    Aplikasi Bank runtime

--- Modul7_1302194084/Modul7_1302194084/Program.cs
+++ Modul7_1302194084/Modul7_1302194084/Program.cs
@@ -1,15 +1,100 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Modul7_1302194084
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
+            BankTransferConfig bankConfig = new BankTransferConfig();
+
+            dynamic conf = bankConfig.ReadJson();
+
+            if (conf.lang == "en")
+            {
+                Console.WriteLine("Please insert the amount of money to transfer:");
+            }
+            else if (conf.lang == "id")
+            {
+                Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+            }
+/////////////////////////////////////
+            string uangDiTranferStr = Console.ReadLine();
+
+            int uangDiTransfer = int.Parse(uangDiTranferStr);
+
+            int biayaTransfer;
+
+            if (uangDiTransfer <= (int)conf.transfer.threshold)
+            {
+                biayaTransfer = conf.transfer.low_fee;
+            }
+            else
+            {
+                biayaTransfer = conf.transfer.high_fee;
+            }
+
+            if (conf.lang == "en")
+            {
+                Console.WriteLine("Transfer fee = " + biayaTransfer);
+                Console.WriteLine("Total amount = " + (biayaTransfer + uangDiTransfer) + "\n");
+                Console.WriteLine("Select transfer method");
+            }
+            else if (conf.lang == "id")
+            {
+                Console.WriteLine("Biaya transfer = " + biayaTransfer);
+                Console.WriteLine("Total biaya = " + (biayaTransfer + uangDiTransfer) + "\n");
+                Console.WriteLine("Pilih metode transfer");
+            }
+/////////////////////////////
+            int index = 0;
+
+            foreach (var mthd in conf.methods)
+            {
+                index++;
+                Console.WriteLine(index + ". " + mthd);
+            }
+
+            Console.WriteLine();
+//////////////////////////////////////
+            string confirm;
+            if (conf.lang == "en")
+            {
+                Console.WriteLine("Please type '" + conf.confirmation.en + "' to confirm the transaction:");
+
+                confirm = Console.ReadLine();
+
+                if (confirm == (string)conf.confirmation.en)
+                {
+                    Console.WriteLine("The transfer is completed");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer is cancelled");
+                }
+            }
+            else if (conf.lang == "id")
+            {
+                Console.WriteLine("Ketik '" + conf.confirmation.id + "' untuk mengkonfirmasi transaksi:");
+
+                confirm = Console.ReadLine();
+
+                if (confirm == (string)conf.confirmation.id)
+                {
+                    Console.WriteLine("Proses transfer berhasil");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer dibatalkan");
+                }
+            }
+
         }
     }
 }
